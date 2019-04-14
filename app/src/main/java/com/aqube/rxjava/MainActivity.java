c5f522a7cc8ce35d4e2c7d46a46d11d02c0b9615
Akhilesh <akhilesh0707@gmail.com>
2019-04-14 23:11:18 +0530

    Operator UI created

--- app/src/main/java/com/aqube/rxjava/MainActivity.java
+++ app/src/main/java/com/aqube/rxjava/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.aqube.rxjava.observables.ObservablesActivity;
+import com.aqube.rxjava.operators.OperatorsActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
--- app/src/main/java/com/aqube/rxjava/operators/OperatorsActivity.java
+++ app/src/main/java/com/aqube/rxjava/operators/OperatorsActivity.java
@@ -1,7 +1,9 @@
-package com.aqube.rxjava;
+package com.aqube.rxjava.operators;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.aqube.rxjava.R;
 
 public class OperatorsActivity extends AppCompatActivity {
 
